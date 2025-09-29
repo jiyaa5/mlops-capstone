6ee224ac93200052eec16fa12506a6f9d7cff69a
jiyaa5 <jiyapatel85853@gmail.com>
2025-09-29 12:20:03 +0530

    Worflow check

--- app.py
+++ app.py
@@ -51,6 +51,3 @@ def predict(data: InputData):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
